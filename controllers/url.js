f4b5f634cb7b15ee841502c4dd5c26ff63dfc07f
Usama Ahmed <usamabinkashif.98@gmail.com>
2023-02-16 03:33:13 +0500

    done

--- controllers/url.js
+++ controllers/url.js
@@ -1,7 +1,7 @@
 const shortid = require("shortid");
 const URL = require("../models/url");
 
-const handleGenerateNewShortURL = async (req, res) => {
+async function handleGenerateNewShortURL(req, res) {
   const body = req.body;
   if (!body.url) return res.status(400).json({ error: "url is required" });
   const shortID = shortid();
@@ -13,24 +13,18 @@ const handleGenerateNewShortURL = async (req, res) => {
   });
 
   return res.json({ id: shortID });
-};
+}
 
-const handleGetURL = async (req, res) => {
-  const date = new Date().toLocaleString();
-
+async function handleGetAnalytics(req, res) {
   const shortId = req.params.shortId;
-  const entry = await URL.findOneAndUpdate(
-    { shortId },
-    {
-      visitHistory: {
-        timestamp: date,
-      },
-    }
-  );
-  return res.redirect(entry.redirectURL);
-};
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
+}
 
 module.exports = {
   handleGenerateNewShortURL,
-  handleGetURL,
+  handleGetAnalytics,
 };
