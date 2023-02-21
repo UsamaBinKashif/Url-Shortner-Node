c21998d34455e0f5ee7b1e8a80c0de7340f7eaee
Usama Ahmed <usamabinkashif.98@gmail.com>
2023-02-21 22:56:03 +0500

    comments

--- connect.js
+++ connect.js
@@ -1,9 +1,11 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", true);
+const mongoose = require("mongoose"); //require mongoose
+mongoose.set("strictQuery", true); 
+//function which takes url as a parameter
 async function connectToMongoDB(url) {
   return mongoose.connect(url);
 }
 
+//export
 module.exports = {
   connectToMongoDB,
 };
