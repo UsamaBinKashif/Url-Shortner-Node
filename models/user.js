ffed268c8120dce6dae4379c191c5ea433ecf6a4
Usama Ahmed <usamabinkashif.98@gmail.com>
2023-02-21 22:48:27 +0500

    comments

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); //intializing moongoose
 
+//creating users schema
 const usersSchema = new mongoose.Schema(
   {
     name: {
@@ -10,13 +11,14 @@ const usersSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
+      unique: true, // this will force user to use a different email if already exist
     },
     password:{
         type: String,
         required: true,
     },
   },
+  // add time as an entry so we can track user logs
   { timestamps: true }
 );
 
