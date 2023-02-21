488029f25a2383245a2c8c8c21371eeea24452b2
Usama Ahmed <usamabinkashif.98@gmail.com>
2023-02-21 22:49:39 +0500

    comments

--- models/url.js
+++ models/url.js
@@ -1,18 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); //intializing moongoose
 
+//creating url schema
 const urlSchema = new mongoose.Schema(
   {
     shortId: {
       type: String,
       required: true,
-      unique: true,
+      unique: true, //force shortid package to create a new id
     },
+    //the url we want to short
     redirectURL: {
       type: String,
       required: true,
     },
     visitHistory: [{ timestamp: { type: Number } }],
   },
+  // add time as an entry so we can track url logs
   { timestamps: true }
 );
 
