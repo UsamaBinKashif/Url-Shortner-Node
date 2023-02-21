346ccd485bd64338cbab9cf6a92b09af4390adb3
Usama Ahmed <usamabinkashif.98@gmail.com>
2023-02-21 22:52:38 +0500

    comments

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,8 @@
-const USERS = require("../models/user");
-const { getUser, setUser } = require("../service/auth");
-const { v4: uuidv4 } = require("uuid");
+const USERS = require("../models/user"); //users schema
+const { getUser, setUser } = require("../service/auth"); //cookie creating from service
+const { v4: uuidv4 } = require("uuid"); // it is an npm package which generate a random id whenever called
 
+//sign up function
 const handleSIGNUPUSER = async (req, res) => {
   const { name, email, password } = req.body;
   await USERS.create({
@@ -9,21 +10,29 @@ const handleSIGNUPUSER = async (req, res) => {
     email,
     password,
   });
+  //if correctly done then user will be added to db and redirected to sign in
   return res.redirect("/signin");
 };
+
+//sign in function
 const handleSIGNINUSER = async (req, res) => {
   const { email, password } = req.body;
   const user = await USERS.findOne({ email, password });
+  //if correctly done then app will find user from db and redirected to home
   if (user) {
     const sessionId = uuidv4();
     setUser(sessionId, user);
     res.cookie("uuid", sessionId);
     return res.redirect("/url");
-  } else {
+
+  } 
+  //else user will be redirected to sign in again
+  else {
     return res.render("signin", {
       error: "Incorrect Email or Password... Try again",
     });
   }
 };
 
+//exporting functions
 module.exports = { handleSIGNINUSER, handleSIGNUPUSER };
