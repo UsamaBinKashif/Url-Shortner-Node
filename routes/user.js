0a6a44227b910d5a052815136b6c288a1066a5e1
Usama Ahmed <usamabinkashif.98@gmail.com>
2023-02-22 23:41:00 +0500

    comments

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,17 @@
-const express = require("express");
-const router = express.Router();
-const { handleSIGNUPUSER } = require("../controllers/user");
+const express = require("express"); //initialize express
+const router = express.Router(); //get express router
+const { handleSIGNUPUSER } = require("../controllers/user"); //signup function
 
+//get req on user
 router.get("/", (req, res) => {
   res.render("signup");
 });
+//get req on user
 router.get("/sigin", (req, res) => {
   res.render("signin");
 });
 
+//post req on user
 router.post("/", handleSIGNUPUSER);
 
 module.exports = router;
