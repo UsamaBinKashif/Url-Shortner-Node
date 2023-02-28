d5e758ce349f7c6272839a35bd94245b4fc085b0
Usama Ahmed <usamabinkashif.98@gmail.com>
2023-02-28 19:16:47 +0500

    jwt-auth

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,20 @@
-const sessionIdToUserMap = new Map();
-
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+const jwt = require("jsonwebtoken");
+const secret = "AliAli110##";
+function setUser(user) {
+  const payload = {
+    _id: user._id,
+    email: user.email,
+    name: user.name,
+  };
+  return jwt.sign(payload, secret);
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = {
